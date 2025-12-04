2a322639c13c4e2c339b027035973acf2f58cd8d
alanraju22 <alanraju205@gmail.com>
2025-12-05 02:03:12 +0530

    Second Commit Footer change

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -24,12 +24,12 @@ export default function Footer() {
           <Typography color="grey.700" variant="h6">
             Developed by{" "}
             <Link
-              href="https://github.com/crazy-man22"
+              href="https://github.com/alanraju22"
               underline="none"
               sx={{ color: "text.primary" }}
               target="_blank"
             >
-              Crazy Man
+              Netflix
             </Link>
           </Typography>
         </Divider>
